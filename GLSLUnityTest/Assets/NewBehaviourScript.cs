cc02f0902c9fc9fc2ced0dabc024481856e51a96
alco6 <44870895+alco6@users.noreply.github.com>
2019-09-25 18:10:16 +0200

    Several tests with GLSL and HLSL

---
+++ GLSLUnityTest/Assets/NewBehaviourScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+    WebCamTexture webcam;
+    Texture2D output;
+    Color32[] data;
+    void Start()
+    {
+       
+        
+            webcam = new WebCamTexture();
+            webcam.Play();
+            output = new Texture2D(webcam.width, webcam.height);
+            GetComponent<Renderer>().material.mainTexture = output;
+            data = new Color32[webcam.width * webcam.height];
+       
+    }
+    void Update()
+    {
+        if (data != null)
+        {
+            webcam.GetPixels32(data);
+            // You can play around with data however you want here.
+            // Color32 has member variables of a, r, g, and b. You can read and write them however you want.
+            output.SetPixels32(data);
+            output.Apply();
+        }
+    }
+ 
+}
